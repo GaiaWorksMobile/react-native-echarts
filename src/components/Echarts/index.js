372d76617df713892e3592033a6bda6ad4536521
Yuki.lan <yuki.lan@gaiaworks.cn>
2018-10-10 10:29:27 +0800

    打包文件过大，所以把html资源挪到原生

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { WebView, View, StyleSheet } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
+import Device from 'react-native-device-detection';
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
+    if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
   }
 
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
-  }
-
   render() {
+    console.log('height is',this.props.height || 400);
+    console.log('width is',this.props.width || Device.width);
     return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
+      <View style={{ flex: 1, height: this.props.height || 400, width: this.props.width || Device.width }}>
         <WebView
           ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
+          scrollEnabled={false}
+          injectedJavaScript={renderChart(this.props)}
           style={{
             height: this.props.height || 400,
+            width: this.props.width || Device.width,
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
-          scalesPageToFit={Platform.OS !== 'ios'}
-          source={Platform.OS === 'ios' ? require('./tpl.html') : {uri:'file:///android_asset/tpl.html'}}
+          scalesPageToFit={!Device.isIos}
+          scrollEnabled={false}
+          source={{uri:'./tpl.html'}}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
